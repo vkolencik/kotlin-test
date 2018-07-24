7d54b2a7bc7aac420a76e1fb736b9174e0ee2bea
Vojtěch Kolenčík <kolencik@gmail.com>
2018-07-25 00:33:27 +0200

    Configure bsic tests

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -2,3 +2,5 @@
 fun main(args : Array<String>) { 
   println("Hello, world!") 
 }
+
+fun sum(a: Int, b: Int) = a + b
---
+++ src/test/kotlin/AppTest.kt
@@ -0,0 +1,16 @@
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Assert.assertEquals
+
+class TraderApplicationTests {
+
+	@Test
+	fun testSumBad() {
+        assertEquals(sum(1, 3), 5)
+	}
+
+	@Test
+	fun testSumGood() {
+        assertEquals(sum(1, 3), 4)
+	}
+}
