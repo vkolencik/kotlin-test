0bec63a2ccf1dbc5ca5e772bffc0d708f7c8b171
Vojtěch Kolenčík <kolencik@gmail.com>
2018-07-26 09:55:37 +0200

    Lint and add linting plugin

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,6 +1,6 @@
 @file:JvmName("App")
-fun main(args : Array<String>) { 
-  println("Hello, world!") 
+fun main(args: Array<String>) {
+    println("Hello, world!")
 }
 
 fun sum(a: Int, b: Int) = a + b
--- src/test/kotlin/TraderApplicationTests.kt
+++ src/test/kotlin/TraderApplicationTests.kt
@@ -1,16 +1,15 @@
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.junit.Assert.assertEquals
 
 class TraderApplicationTests {
 
-	/*@Test
+    /*@Test
 	fun testSumBad() {
         assertEquals(sum(1, 3), 5)
 	}*/
 
-	@Test
-	fun testSumGood() {
+    @Test
+    fun testSumGood() {
         assertEquals(sum(1, 3), 4)
-	}
+    }
 }
